d3b3835a661cbf1d135b32ca829fb4cb2c7bc546
Ali <a80.abbasi@yahoo.com>
2022-07-26 19:55:09 +0430

    Added TFIDF and Boolean search

---
+++ TFIDF/TFIDF.py
@@ -0,0 +1,33 @@
+from PreProcess.PreProcess import sent_pre_process
+from Utils.Utils import boostan_data, beit_based_all, poem_based_boostan, poem_based_all
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+
+class TFIDF:
+    def __init__(self, Golestan=False, poem_based=False):
+        if poem_based:
+            if Golestan:
+                self.data = poem_based_all
+            else:
+                self.data = poem_based_boostan
+        else:
+            if Golestan:
+                self.data = beit_based_all
+            else:
+                self.data = boostan_data
+
+        self.tfidf = TfidfVectorizer(analyzer=lambda x: sent_pre_process(x, remove_stopwords=True))
+        tfidf_data = self.tfidf.fit_transform(self.data)
+        self.tfidf_data = tfidf_data.toarray()
+
+    def get_vectors(self):
+        return self.tfidf_data
+
+    def search(self, query, k=10):
+        query_tfidf = self.tfidf.transform([query]).toarray()[0]
+        scores = np.dot(self.tfidf_data, query_tfidf)  # their inner products
+        args = np.argsort(scores)[::-1]
+        k_args = args[:k]
+        return self.data.poem.to_numpy()[k_args]
--- Utils/Utils.py
+++ Utils/Utils.py
@@ -3,7 +3,6 @@ import pandas as pd
 import re
 import string
 
-
 boostan_data = list()
 for chapter_name in os.listdir('../resources/boostan'):
     for file_name in os.listdir(f'../resources/boostan/{chapter_name}'):
@@ -17,6 +16,7 @@ for chapter_name in os.listdir('../resources/boostan'):
 boostan_data = pd.DataFrame(boostan_data)
 boostan_data.columns = ['poem', 'chapter', 'section']
 boostan_data.sort_values(by=['chapter', 'section'], ignore_index=True, inplace=True)
+boostan_data['book'] = 'B'
 
 golestan_data = list()
 for chapter_name in os.listdir('../resources/golestan'):
@@ -35,6 +35,7 @@ for chapter_name in os.listdir('../resources/golestan'):
 golestan_data = pd.DataFrame(golestan_data)
 golestan_data.columns = ['poem', 'chapter', 'section']
 golestan_data.sort_values(by=['chapter', 'section'], ignore_index=True, inplace=True)
+golestan_data['book'] = 'G'
 
 all_data = pd.concat([boostan_data, golestan_data], ignore_index=True)['poem']
 
@@ -55,3 +56,13 @@ with open('../resources/replace.txt', encoding='utf-8') as f:
         key, value = key.strip(), value.strip()
         replace_dict[f'{key}'] = f'{value}'
         line = f.readline()
+
+poem_based_boostan = boostan_data.groupby(['book', 'chapter', 'section']).apply(
+    lambda f: '\n'.join(f['poem'].tolist())).reset_index()
+
+poem_based_golestan = golestan_data.groupby(['book', 'chapter', 'section']).apply(
+    lambda f: '\n'.join(f['poem'].tolist())).reset_index()
+
+poem_based_all = pd.concat([poem_based_boostan, poem_based_golestan], ignore_index=True)
+
+beit_based_all = pd.concat([boostan_data, golestan_data], ignore_index=True)
